1f1e34f78b3e7c209cf5dc646d81a8ab77b95a67
QYongCN <40787180+QYongCN@users.noreply.github.com>
2019-08-21 16:35:02 +0800

    Add files via upload

--- BilinearLayerLinear.py
+++ BilinearLayerLinear.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.optim as optim
+from visdom import Visdom
 
 class BL(nn.Module):
     def __init__(self, output_dim, input_dim, template):
@@ -16,14 +17,15 @@ class BL(nn.Module):
         self.template = template
         self.W11 = nn.Parameter(torch.Tensor(template[1][0], input_dim[0]), requires_grad=True)
         self.W12 = nn.Parameter(torch.Tensor(output_dim[0], template[1][0]), requires_grad=True)
-        self.fc1 = nn.Linear(self.input_dim[0], self.template[1][0], bias=False)
+        # self.fc1 = nn.Linear(self.input_dim[0], self.template[1][0], bias=False)
         self.fc2 = nn.Linear(self.input_dim[1], self.template[1][1],  bias=False)
-        self.fc3 = nn.Linear(self.template[1][0], self.output_dim[0], bias=False)
+        # self.fc3 = nn.Linear(self.template[1][0], self.output_dim[0], bias=False)
         self.fc4 = nn.Linear(self.template[1][1], self.output_dim[1], bias=False)
         self.bias1 = nn.Parameter(torch.Tensor(template[1][0], template[1][1]), requires_grad=True)
         self.bias2 = nn.Parameter(torch.Tensor(output_dim[0], output_dim[1]), requires_grad=True)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.Softmax()
+        # self.dropout = nn.Dropout(0.1)
 
 
     def forward(self, x):
@@ -40,8 +42,10 @@ class BL(nn.Module):
        for i in range(x.shape[0]):
            item2[i] = item2[i] + self.bias1
 
-       item2 = self.relu(item2)
-       item2 = self.dropout(item2)
+       # item2 = self.relu(item2)
+       item2 = self.softmax(item2)
+       # item2 = self.dropout(item2)*100
+
 
        item3 = torch.Tensor(x.shape[0], self.output_dim[0], self.template[1][1])
        for i in range(x.shape[0]):
@@ -53,6 +57,8 @@ class BL(nn.Module):
 
        for i in range(x.shape[0]):
            item4[i] = item4[i] + self.bias2
+
+
 
        out = torch.Tensor(x.shape[0], self.output_dim[0])
        if self.output_dim[1] == 1:
@@ -74,6 +80,7 @@ x = torch.from_numpy(x)
 input = x.type(torch.DoubleTensor)
 input_mean, input_std = torch.mean(x, dim=0), torch.std(x, dim=0)
 input_norm = (input - input_mean) / input_std
+# print(input_norm*10)
 
 
 label = np.random.randint(0, 3, (1000, ))
@@ -84,7 +91,8 @@ y = ones.index_select(0, item.long())
 target = y
 
 loss_list = []
-epochs = 3
+acc_list = []
+epochs = 200
 
 model = BL(template[-1],template[0],template)
 
@@ -96,20 +104,30 @@ model = BL(template[-1],template[0],template)
 criterion = nn.BCEWithLogitsLoss()
 # criterion = nn.CrossEntropyLoss()
 # optimizer = optim.Adam(model.parameters(), lr = 0.02)
-optimizer = optim.SGD(model.parameters(), lr=0.0000002, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.9)
+
+viz = Visdom()
+viz .line([0.], [0], win='train', opts=dict(title="loss", legend=['loss']))
 
 for epoch in range(epochs):
     print("epoch:",epoch)
     # output=model(x.float())
-    output=model(input_norm.float())
+    output=model(input_norm.float()*10)
 
     loss = criterion(output, target)
     print(loss.item())
     loss_list.append(loss.item())
 
+    viz.line([loss.item()], [epoch], win='train_loss', update='append')
+
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+    # for item in model.named_parameters():
+    #     if item[0] == 'fc2.weight':
+    #         h = item[1].register_hook(lambda grad: print(grad))
+    #         print(h)
 
 
 
